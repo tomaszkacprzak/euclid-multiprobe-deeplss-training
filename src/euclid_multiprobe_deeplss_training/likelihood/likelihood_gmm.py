293554769668ddd767bb44158cbe507611de404b
Tomasz Kacprzak <tomasz.kacprzak@fhnw.ch>
2026-09-21 13:17:04 +0200

    Added likelihood module stubs

---
+++ src/euclid_multiprobe_deeplss_training/likelihood/likelihood_gmm.py
@@ -0,0 +1,6 @@
+import warnings
+from euclid_multiprobe_deeplss_training.utils import logger
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+warnings.filterwarnings("once", category=UserWarning)
+LOGGER = logger.get_logger(__file__)
